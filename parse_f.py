b08f9ace9312f8353ed6730fa0fde5e42080b1e2
ddrppx <ddrppx@gmail.com>
2021-06-05 16:52:37 -0300

    Final?

--- parse_f.py
+++ parse_f.py
@@ -7,14 +7,20 @@ file_status = os.path.isfile(path)
 if(file_status):
         # Arrays for storing data
     contents = []
-        # Opening the filed  
+    important = []
+        # Opening the file  
     with open(path) as f:
         contents = f.readlines()
+        for line in contents:
+            if '->' in line:
+                important = contents
+
+    f.close()
 
     count = 0
     output = []
         # Going through file contents
-    for content in contents:
+    for content in important:
         count += 1
             # Splitting values for another array loop
         splitted = content.split()
@@ -25,5 +31,9 @@ if(file_status):
             if (search >= 0):
                 output = index
                 print(f'{count}: {output}')
+        
+        # Empty output
+    if not output:
+        print('Nothing to show from file.')
 else:
     print(f'File not found.\nCheck the inserted path: {path}')
